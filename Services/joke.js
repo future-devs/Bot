86e2c767f29a5f7ac99cd02f43f60aae0bfaaee1
darksun27 <sbsiddharth@gmail.com>
2019-05-27 19:31:51 +0530

    update reply

--- Services/joke.js
+++ Services/joke.js
@@ -5,8 +5,7 @@ function getRandomJoke(callback, userId){
         url : process.env.JOKE_API
     }, (error, response, body)=>{
         if(!error && response.statusCode == 200){
-            var reply = `a joke specially for you hope it makes your day better :smile:\n
-                        ${JSON.parse(body).setup}\n${JSON.parse(body).punchline}`;
+            var reply = `a joke specially for you hope it makes your day better :smile:\n${JSON.parse(body).setup}\n${JSON.parse(body).punchline}`;
             return callback(reply, userId);
         }
         else{
