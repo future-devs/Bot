6dc86e8ee70fd366f165115599787d4794b34498
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-28 13:06:30 +0530

    Update requestAPI.js
    install axios

--- Handlers/requestAPI.js
+++ Handlers/requestAPI.js
@@ -1,25 +1,19 @@
 require('dotenv').config();
-const request = require('request');
+const axios = require('axios');
 
 function getMembers(){
     var list = {};
-    request({
-        url : 'https://discordapp.com/api/v6/guilds/526630690612183050/members',
-        headers : {
-            Authorization : `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-        },
-        qs : {
-            limit : 1000
-        },
-        method : 'GET'
-    },async function(e,r,b){
-        list = await JSON.parse(b);
+    axios.get('https://discordapp.com/api/v6/guilds/526630690612183050/members',
+              {headers: {
+          "Authorization" : `Bot ${process.env.DISCORD_BOT_TOKEN}`
+        }
+      }
+             ).then((res)=>{
+        list = JSON.parse(res);
         console.log(list);
-    });
-    console.log('hello');
-    console.log(list);
-    return list;
-}
+    }
+    
+    )
 
 module.exports = {
     getMembers
