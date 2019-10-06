9b906d41c4b95f75df585a2a305227331b344d5f
Siddharth Batra <sbsiddharth@gmail.com>
2019-10-07 00:05:48 +0530

    Update facts.js
    Updated format for displaying a fact about a random number

--- Services/facts.js
+++ Services/facts.js
@@ -3,5 +3,17 @@ const request = require('request');
 function generateRandomFact(userId){
     request({
         url : NUMBER_FACTS_API
+    },(error, response, body)=>{
+        if(!error && response.statusCode == 200){
+            var reply = `A fact about number {JSON.parse(body).number} just for you:smile:\n${JSON.parse(body).text}`;
+            return callback(reply, userId);
+        }
+        else{
+            console.log(error);
+        }
     })
-}
+})
+
+module.exports = {
+    generateRandomFact
+};
